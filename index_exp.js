e31ad67f30d1d677689bed6f71b8e09308ba95e7
bugula <bugula@gmail.com>
2023-04-06 21:01:49 -0400

    Add files via upload

--- index_exp.js
+++ index_exp.js
@@ -32,66 +32,45 @@ for (var i = 0; i < items.length; i++) {
 	});
 }
 
-function filterItems() {
-	var finishFilter = document.getElementById("finish-filter").value;
-	console.log(finish, 'keith did this')
-  
-	if (finishFilter === "") {
-	  // Show all items if no filters are selected
-	  showAllItems();
-	} else {
-	  // Filter items based on selected finish
-	  var filteredItems = items.filter(function(item) {
-		return item.finish.toLowerCase().indexOf(finishFilter.toLowerCase()) !== -1;
-	  });
-	  updateItemList(filteredItems);
-	}
-  }
-
-function filterItemsByType(type) {
-	var filteredItems;
-	if (type === '') {
-	  filteredItems = items;
-	} else {
-	  filteredItems = items.filter(function(item) {
-		return item.type === type;
-	  });
-	}
-	updateItemList(filteredItems);
-}
-
-
-function showAllItems() {
-	updateItemList(items);
-}
-
-function updateItemList(updatedItems) {
-	var itemList = document.getElementById("item-list");
+// Add this to your JavaScript code
+function filterByFinish() {
+	var selectedFinish = document.getElementById("finish-filter").value;
+	var filteredItems = items.filter(item => item.finish === selectedFinish || selectedFinish === "");
 	itemList.innerHTML = "";
-
-	for (var i = 0; i < updatedItems.length; i++) {
-		var item = updatedItems[i];
-		var itemDiv = document.createElement("div");
-		itemDiv.classList.add("card");
-		itemDiv.innerHTML = `
-    		<h2>${item.name}</h2>
-			<div class="card-content">
-			<div class="card-image">
-        		${item.image ? `<img src="images/${item.image}" width="100" height="100">` : ""}
-    		</div>
-			<div class="card-text">
-    			${item.barrel}<br>
-    			${item.type}<br>
-    			${item.finish}
-			</div>
-			</div>
-    		<a href="javascript:viewItemDetails(${item.id})">View Details</a>
-			</div>
-			`;
-		itemList.appendChild(itemDiv);
-	  }
-}
-
+	filteredItems.forEach(item => {
+	  var itemDiv = document.createElement("div");
+	  itemDiv.classList.add("card");
+	  itemDiv.innerHTML = `
+		<h2>${item.name}</h2>
+		<div class="card-content">
+		  <div class="card-image">
+			${item.image ? `<img src="images/${item.image}" width="100" height="100">` : ""}
+		  </div>
+		  <div class="card-text">
+			${item.barrel}<br>
+			${item.type}<br>
+			${item.finish}
+		  </div>
+		</div>
+		<a href="javascript:viewItemDetails(${item.id})">View Details</a>
+	  `;
+	  itemList.appendChild(itemDiv);
+  
+	  itemDiv.addEventListener("click", function() {
+		viewItemDetails(item.id);
+	  });
+	});
+  }
+  
+  function getFinishOptions() {
+	var finishes = new Set(items.map(item => item.finish));
+	var options = "";
+	finishes.forEach(finish => {
+	  options += `<option value="${finish}">${finish}</option>`;
+	});
+	return options;
+  }
+  
 
 // GENERATE LINK TO DETAILS PAGE
 function viewItemDetails(itemId) {
